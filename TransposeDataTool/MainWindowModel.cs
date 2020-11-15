4892cfdad06a0275be24a891c81b9d8f2465c851
alina <rottmannalina96@gmail.com>
2020-11-15 16:35:32 +0100

    Init Transpose Data Tool

---
+++ TransposeDataTool/MainWindowModel.cs
@@ -0,0 +1,85 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace TransposeDataTool
+{
+    internal class MainWindowModel
+    {
+        internal string[,] TransposeData(string[,] data)
+        {
+            var rows = data.GetLength(0);
+            var columns = data.GetLength(1);
+            var transposed = new string[columns, rows];
+
+            for (var column = 0; column < columns; column++)
+            {
+                for (var row = 0; row < rows; row++)
+                {
+                    //Console.WriteLine($"Data at {column} | {row} : {data[row, column]}");
+                    transposed[column, row] = data[row, column];
+                }
+            }
+
+            return transposed;
+        }
+
+        internal void SaveData(string fileName, string[,] data)
+        {
+            var transposedFileName = Path.GetDirectoryName(fileName) + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(fileName) + "_transposed.csv";
+
+            using (StreamWriter file = new StreamWriter(transposedFileName))
+            {
+                for (var row = 0; row < data.GetLength(0); row++)
+                {
+                    for (var column = 0; column < data.GetLength(1); column++)
+                    {
+                        file.Write(data[row, column] + ';');
+                    }
+                    file.WriteLine();
+                }
+            }
+
+
+            MessageBox.Show($"Data successfully transposed into {transposedFileName}");
+        }
+
+        internal string[,] ReadFile(string fileName)
+        {
+            var rows = 0;
+            var columns = 0;
+
+            using (var reader = new StreamReader(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    rows++;
+                    var values = reader.ReadLine().Split(';');
+                    values = values.Take(values.Count() - 1).ToArray();
+                    columns = values.Length;
+                }
+            }
+
+            var data = new string[rows, columns];
+
+            using (var reader = new StreamReader(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    for (var row = 0; row < rows; row++)
+                    {
+                        var values = reader.ReadLine().Split(';');
+                        values = values.Take(values.Count() - 1).ToArray();
+
+                        for (var column = 0; column < columns; column++)
+                        {
+                            data[row, column] = values[column];
+                        }
+
+                    }
+                }
+            }
+            return data;
+        }
+    }
+}
---
+++ TransposeDataTool/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+using Reactive.Bindings;
+
+namespace TransposeDataTool
+{
+    internal class MainWindowViewModel
+    {
+        private readonly MainWindowModel mainWindowModel;
+
+        public MainWindowViewModel()
+        {
+            this.mainWindowModel = new MainWindowModel();
+            this.OpenFileButton = new ReactiveCommand();
+            this.OpenFileButton.Subscribe(this.OpenFile_Click);
+        }
+
+        private void OpenFile_Click()
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // receive file name
+                var fileName = openFileDialog.FileName;
+
+                // read data
+                var data = this.mainWindowModel.ReadFile(fileName);
+
+                // transpose data content 
+                var transposedData = this.mainWindowModel.TransposeData(data);
+
+                // save transposed table into new file
+                this.mainWindowModel.SaveData(fileName, transposedData);
+            }
+        }
+
+        public ReactiveCommand OpenFileButton { get; }
+    }
+}
